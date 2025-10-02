cdff466277211177aed5e774e6b1526da450fe56
Student <nigelmraydavey@gmail.com>
2025-10-02 10:06:32 -0700

    Changed values

--- src/main/java/org/nigel/CellPhone.java
+++ src/main/java/org/nigel/CellPhone.java
@@ -1,4 +1,60 @@
 package org.nigel;
 
 public class CellPhone {
+
+    private String PhoneNumber;
+    private String Model;
+    private String Carrier;
+    private String Owner;
+    private String SerialNumber;
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+
+    public String getModel() {
+        return Model;
+    }
+
+    public void setModel(String model) {
+        Model = model;
+    }
+    public String getCarrier() {
+        return Carrier;
+    }
+
+    public void setCarrier(String carrier) {
+        Carrier = carrier;
+    }
+    public String getOwner() {
+        return Owner;
+    }
+
+    public void setOwner(String owner) {
+        Owner = owner;
+    }
+
+    public String getSerialNumber() {
+        return SerialNumber;
+    }
+
+    public void setSerialNumber(String sn) {
+        SerialNumber = sn;
+    }
+
+    public CellPhone() {
+        this.PhoneNumber = null;
+        this.Model = null;
+        this.Carrier = null;
+        this.Owner = null;
+    }
+
+    public void dial(String PhoneNumber) {
+        System.out.printf(getOwner() + "'s phone is calling " + PhoneNumber + "\n");
+
+    }
 }
--- src/main/java/org/nigel/Main.java
+++ src/main/java/org/nigel/Main.java
@@ -1,17 +1,74 @@
 package org.nigel;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+public class Main {
+    static void main2() {
+        CellPhone phone = new CellPhone();
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("What is the serial number? ");
+        phone.setSerialNumber(scan.nextLine());
+
+        System.out.print("What model number? ");
+        phone.setModel(scan.nextLine());
+
+        System.out.print("Who is the carrier? ");
+        phone.setCarrier(scan.nextLine());
+
+        System.out.print("What is the phone number? ");
+        phone.setPhoneNumber(scan.nextLine());
+
+        System.out.print("Who is the owner ");
+        phone.setOwner(scan.nextLine());
+
+        System.out.println("==Output==");
+        System.out.println("Phone number: " + phone.getPhoneNumber());
+        System.out.println("Model Phone: " + phone.getModel());
+        System.out.println("Carrier: " + phone.getCarrier());
+        System.out.println("Serial Number: " + phone.getSerialNumber());
+        System.out.println("Owner: " + phone.getOwner());
+
+    }
+
+    static void main() {
+        CellPhone phone1 = new CellPhone();
+
+        phone1.setSerialNumber("123123123123123");
+        phone1.setModel("Iphone 16");
+        phone1.setCarrier("T-mobile");
+        phone1.setPhoneNumber("3_852_319_54");
+        phone1.setOwner("Nigel Davey");
+        phone1.dial("206-231-54545");
+
+        CellPhone phone2 = new CellPhone();
+
+        phone2.setSerialNumber("9999999999");
+        phone2.setModel("Iphone 15");
+        phone2.setCarrier("T-mobile");
+        phone2.setPhoneNumber("231_252_232");
+        phone2.setOwner("rando");
+        phone2.dial("2323131231231");
+
+        display(phone1);
+        display(phone2);
+        phone1.dial(phone2.getPhoneNumber());
+        phone2.dial(phone1.getPhoneNumber());
+
+    }
+
+    public static void display(CellPhone phone) {
+        System.out.println();
+
+        System.out.printf("PhoneNumber: %s", phone.getPhoneNumber());
+        System.out.println();
+        System.out.printf("SerialNumber: %s", phone.getSerialNumber());
+        System.out.println();
+        System.out.printf("Model: %s", phone.getModel());
+        System.out.println();
+        System.out.printf("Owner: %s", phone.getOwner());
+        System.out.println();
+
+
     }
 }
